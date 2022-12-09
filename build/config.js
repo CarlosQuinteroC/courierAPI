0e6abcfc3eca9fd413442b01c5bced5fa23bb16c
Daniel Millan <dani.millan07@gmail.com>
2022-12-08 19:45:42 -0500

    Delete innecesary folders

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: process.env.PORT || 4500,
-    databaseString: process.env.DB_STRING || 'mongodb://127.0.0.1:27017/CourierAPP',
-    jwtKey: process.env.JWT_KEY || "$f24tC1234tr#rf3",
-};
